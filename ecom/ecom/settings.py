ba96cdd23fc422c3e7fe1bfadb09290a009f5b17
m-vp <mvenkatprashanth1@gmail.com>
2025-03-30 23:50:41 +0530

    settings.py db endpoint for AWS update

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'ecommerce',
         'USER': 'master',
         'PASSWORD': 'masterpassword',
-        'HOST': 'ecommerce.ce2bp6jj4rwp.us-east-1.rds.amazonaws.com',
+        'HOST': 'ecommerce.cjqsc4skqzet.us-east-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
